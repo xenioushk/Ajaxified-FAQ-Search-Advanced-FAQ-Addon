91aa6f8a953a31cb37d6d411c3c0c1afa66d0303
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-05-13 00:23:46 +0300

    🔥 Updated: Options panel Settings.

--- src/admin/modules/admin.js
+++ src/admin/modules/admin.js
@@ -3,12 +3,10 @@
     // Initialize the color picker for the specified fields
     const colorPickerFields = ["#afs_search_window_color", "#afs_sticky_container_bg", "#afs_sugg_box_container_bg", "#afs_sugg_box_text_color", "#afs_sugg_box_text_hover_color"]
 
-    colorPickerFields.forEach(function (selector) {
+    colorPickerFields.forEach((selector) => {
       // Initialize the color picker for each field
       if ($(selector).length) {
         $(selector).wpColorPicker()
-      } else {
-        console.warn(`Element ${selector} not found for color picker initialization.`)
       }
     })
   })
